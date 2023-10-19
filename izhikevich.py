724e52505f746eb47918e9563fc76700d1b5d7a9
MartinDxt <martinbreccia1995@gmail.com>
2023-10-19 15:17:47 +0200

    Izhikevich implementation of 1 neuron

--- izhikevich.py
+++ izhikevich.py
@@ -84,15 +84,7 @@ def unullcline(V, Vr, b):
     return b * (V - Vr)
 
 
-def U_vec(X, Y, Vr, Vt, k, I, C):
-    return (np.full(X.shape, k) * (X - np.full(X.shape, Vr)) * (X - np.full(X.shape, Vt)) - Y + np.full(X.shape, I))/np.full(X.shape, C)
-
-
-def V_vec(X, Y, Vr, a, b):
-    return np.full(X.shape, a)*(np.full(X.shape, b)*(X - np.full(X.shape, Vr)) - Y)
-
-
-v = np.linspace(-70, 35, 1000)
+v = np.linspace(-500, 500, 2000)
 
 # Define initial parameters
 Vr = -60
@@ -108,12 +100,9 @@ C = 100
 s = Sim(Vr, Vt, Vpeak, a, b, c, d, k, I, C)
 x1 = [0] * 100
 y1 = [0] * 100
-x2 = np.arange(-70, 35, 1)
+x2 = np.arange(-1000, 0, 1)
 y2 = [0] * x2.size
 i = [-1.5] * x2.size
-x = np.arange(-70, 35, 5)
-y = np.arange(-50, 60, 5)
-X, Y = np.meshgrid(x, y)
 
 # Create the figure and the line that we will manipulate
 fig, ((ax1, ax2), axs) = plt.subplots(2, 2, figsize=(15, 10))
@@ -125,9 +114,6 @@ ax1.plot(v, Vnullcline(v, Vr, Vt, k, I), lw=2)
 ax1.plot(v, unullcline(v, Vr, b), lw=2)
 ax1.set_xlabel('V')
 ax1.set_ylabel('w')
-U, V = U_vec(X, Y, Vr, Vt, k, I, C), V_vec(X, Y, Vr, a, b)
-ax1.quiver(X, Y, U, V)
-print(U)
 # setting limits to the axes
 ax1.set_xlim((-70, 35))
 ax1.set_ylim((-50, 60))
@@ -186,8 +172,8 @@ axI = fig.add_axes([0.5, 0.1, 0.35, 0.03])
 I_slider = Slider(
     ax=axI,
     label='I',
-    valmin=-100,
-    valmax=100,
+    valmin=-1000,
+    valmax=1000,
     valinit=I,
 )
 
@@ -247,16 +233,7 @@ def update(val):
     ax1.clear()
     ax1.plot(v, Vnullcline(v, Vr_slider.val, Vt_slider.val, k_slider.val, I_slider.val), lw=2)
     ax1.plot(v, unullcline(v, Vr_slider.val, b_slider.val), lw=2)
-    U, V = (U_vec(X, Y, Vr_slider.val, Vt_slider.val, k_slider.val, I_slider.val, C_slider.val),
-            V_vec(X, Y, Vr_slider.val, a_slider.val, b_slider.val))
-    ax1.quiver(X, Y, U, V)
-
-    ax1.set_xlim((-70, 35))
-    ax1.set_ylim((-50, 60))
     fig.canvas.draw_idle()
-    ax2.set_xlim((-70, 35))
-    ax2.set_ylim((-50, 60))
-    ax3.set_ylim((-70, 35))
     s.updatevar(Vr_slider.val, Vt_slider.val, Vpeak_slider.val, a_slider.val, b_slider.val, c_slider.val, d_slider.val, k_slider.val, I_slider.val, C_slider.val)
 
 
@@ -271,12 +248,17 @@ d_slider.on_changed(update)
 k_slider.on_changed(update)
 I_slider.on_changed(update)
 C_slider.on_changed(update)
+
 # Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
-resetax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
-button = Button(resetax, 'Reset', hovercolor='0.975')
+rs_ib = fig.add_axes([0.8, 0.025, 0.1, 0.04])
+button_ib = Button(rs_ib, 'Intrinsically bursting', hovercolor='0.975')
+rs_ax = fig.add_axes([0.1, 0.025, 0.1, 0.04])
+button_rs = Button(rs_ax, 'Regular spiking', hovercolor='0.975')
+rs_ch = fig.add_axes([0.3, 0.025, 0.1, 0.04])
+button_ch = Button(rs_ch, 'chattering', hovercolor='0.975')
 
 
-def reset(event):
+def regular_spiking(event):
     Vr_slider.reset()
     Vt_slider.reset()
     Vpeak_slider.reset()
@@ -287,27 +269,44 @@ def reset(event):
     k_slider.reset()
     I_slider.reset()
     C_slider.reset()
+
+def intrinsically_bursting(event):
+    Vr_slider.set_val(-75)
+    Vt_slider.set_val(-45)
+    Vpeak_slider.set_val(50)
+    a_slider.set_val(0.01)
+    b_slider.set_val(5)
+    c_slider.set_val(-56)
+    d_slider.set_val(130)
+    k_slider.set_val(1.2)
+    C_slider.set_val(150)
+
+def chattering(event):
+    Vr_slider.set_val(-60)
+    Vt_slider.set_val(-40)
+    Vpeak_slider.set_val(25)
+    a_slider.set_val(0.03)
+    b_slider.set_val(5)
+    c_slider.set_val(-40)
+    d_slider.set_val(150)
+    k_slider.set_val(1.5)
+    C_slider.set_val(50)
 """
-def integrator(event):
-    Vr_slider.set_val()
-    Vt_slider.set_val()
-    Vpeak_slider.set_val()
-    a_slider.set_val()
-    b_slider.set_val()
-    c_slider.set_val()
-    d_slider.set_val()
-    k_slider.set_val(0.7)
-    C_slider.set_val(100)
+dV/dt  = (k*(V - Vr)*(V - Vt) - u + I)/C
+du/dt = a*(b*(V - Vr) - u)
 """
 
 def animate(args):
     ax2.clear()
     ax3.clear()
-    ax1.set_xlim((-70, 35))
-    ax1.set_ylim((-50, 60))
-    ax2.set_xlim((-70, 35))
-    ax2.set_ylim((-50, 60))
-    ax3.set_ylim((-70, 35))
+    ax1.set_ylim((-500, 1000))
+    ax1.set_xlim((-300, 300))
+    ax2.set_ylim((-500, 1000))
+    ax2.set_xlim((-300, 300))
+    """
+    ax2.set_xlim((-Vpeak_slider.val*2, Vpeak_slider.val))
+    ax2.set_ylim((-75, 100))
+    ax3.set_ylim((-Vpeak_slider.val, Vpeak_slider.val))"""
     x1.pop(0)
     y1.pop(0)
     y2.pop(0)
@@ -319,6 +318,8 @@ def animate(args):
     return ax2.plot(x1, y1, color='g'), ax3.plot(x2, y2, color='b'), ax3.plot(x2, i, color='r'), plt.show()
 
 
-button.on_clicked(reset)
+button_rs.on_clicked(regular_spiking)
+button_ib.on_clicked(intrinsically_bursting)
+button_ch.on_clicked(chattering)
 anim = animation.FuncAnimation(fig, animate, frames=frames, interval=30, save_count=2000)
 plt.show()
